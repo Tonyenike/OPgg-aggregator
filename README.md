23868df69664a0b503b54829b97b81568d5a8d61
tonyenike <martinb3@flip1.engr.oregonstate.edu>
2019-03-18 21:43:11 -0700

    Added more features, fixed the Remake bug

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # OPgg-aggregator
 
-This tool calculates the average of all the viewable Ranked Flex 5v5 OP scores in the OP.GG match history.
+This tool calculates the average of all the viewable OP scores in the OP.GG match history.
 
 Directions for use:
 
-`python opggscorecalculator.py [options] -u [summoner-name-1] -u [summoner-name-2] -u [summoner-name-3]` ...
+`python opggscorecalculator.py [options]`
 
-If no users are specified, then by default, the aggregator will calculate the OPgg scores of the following summoners:
+If no users are specified in the options, then by default, the aggregator will calculate the op.gg scores of the following summoners:
 * MAN OF INTELLECT
 * Commandments
 * MunichMonster
@@ -18,7 +18,17 @@ If no users are specified, then by default, the aggregator will calculate the OP
 
 `--verbose`
 
-prints more information when gathering OPgg information.
+outputs more information when gathering op.gg data.
+
+`--grouped`
+
+Only selects games where all users that were included in the options are participating in the games.
+
+`-u [summonername]`
+
+Selects the summoner to average their op.gg score. Multiple summoners can be selected by doing `-u [summoner1] -u [summoner2] -u [summoner3]` ... 
+
+Space characters in the summoner's name must be replaced with underscore characters. Names are not case-sensitive.
 
 
 `-r [region]`
@@ -40,3 +50,12 @@ specifies the region. Valid regions are:
 `-l [limit-number]`
 
 specifies a cap on the past number of games to calculate. For example, `-l 10` would limit the aggregate calculation to the last 10 games.
+
+`-m [game-mode]`
+
+specifies the game mode. Valid game modes are:
+
+* solo
+* flex
+* norm
+* ranked -- includes games from both flex and solo.
--- opggcalculator.py
+++ opggcalculator.py
@@ -8,19 +8,39 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 verbose_mode = False
+grouped_mode = False
 
 names = []
 
 specified = False
 limited = False
 region = "na"
+mode = "Flex 5:5 Rank"
+printable_mode = "Ranked Flex"
 limitnum = 10
 for opts in range(len(sys.argv)):
     if sys.argv[opts] == "--verbose":
         verbose_mode = True
+    if sys.argv[opts] == "--grouped":
+        grouped_mode = True
     if sys.argv[opts] == "-u":
         specified = True
-        names.append(sys.argv[opts + 1])
+        stringified = sys.argv[opts + 1]
+        names.append(stringified.replace('_', ' '))
+    if sys.argv[opts] == "-m":
+        choice = sys.argv[opts + 1]
+        if "flex" in choice.lower():
+            mode = "Flex 5:5 Game"
+            printable_mode = "Ranked Flex"
+        elif "solo" in choice.lower():
+            mode = "Ranked Solo"
+            printable_mode = "Ranked Solo"
+        elif "norm" in choice.lower():
+            mode = "Normal"
+            printable_mode = "Normal"
+        elif "rank" in choice.lower():
+            mode = "Rank"
+            printable_mode = "Ranked"
     if sys.argv[opts] == "-r":
         region = sys.argv[opts + 1]
         if region == "kr":
@@ -39,13 +59,15 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 path_to_extension = './3.41.0_0'
 chrome_options.add_argument('load-extension=' + path_to_extension)
+# chrome_options.binary_location = './google-chrome'
 
 print('')
 print('')
 
 for j in range(len(names)):
    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path = './chromedriver')
-   print("Loading information for user: " + names[j])
+   if verbose_mode:
+        print("Loading information for user: " + names[j])
    driver.get('https://' + region + '.op.gg/summoner/userName=' + names[j])
 
    time.sleep(3)
@@ -63,10 +85,11 @@ for j in range(len(names)):
         except:
             goodbool = False
 
-        time.sleep(3)
+        time.sleep(2)
 
    buttons = driver.find_elements_by_class_name('MatchDetail')
    yote = driver.find_elements_by_class_name('GameType')
+   result_detail = driver.find_elements_by_class_name('GameResult')
    realstuff = driver.find_element_by_class_name('RealContent')
    if verbose_mode:
         print("Found " + str(len(yote)) + " games in total")
@@ -74,30 +97,41 @@ for j in range(len(names)):
    totalscore = 0
    for i in range(len(yote)):
       yeet = yote[i].get_attribute('innerHTML')
-      bol = "Flex 5:5 Rank" in yeet
+      resultinfo = result_detail[i].get_attribute('innerHTML')
+      bol = (mode in yeet) and (not ("Remake"  in resultinfo))
       if bol:
-         if verbose_mode:
-                print("Ranked Flex 5v5 game " + str(total + 1) + ":")
          actions = webdriver.ActionChains(driver)
-         time.sleep(1)
          actions.move_to_element(buttons[i]).click().perform()
          time.sleep(1)
          getName = realstuff.find_elements_by_css_selector('td.SummonerName.Cell')
          getScores = driver.find_elements_by_css_selector('div.OPScore.Text')
-         for h in range(10):
-            username = getName[total*10 + h].find_element_by_tag_name('a').get_attribute('innerHTML')
-            if verbose_mode:
-                print(username + " " + getScores[total*10 + h].get_attribute('innerHTML'))
-            if names[j] in username:
-                 totalscore = totalscore + float(getScores[total*10 + h].get_attribute('innerHTML'))
-         total = total + 1
-         if verbose_mode:
-            print("")
-         if limited and limitnum == total:
-            break;
+         subset = getName[-10:]
+         namestuff = []
+         for hi in range(10):
+            namestuff.append(subset[hi].find_element_by_tag_name('a').get_attribute('innerHTML'))
+         namesG = [x.lower() for x in namestuff]
+         othernames = [x.lower() for x in names]
+         goodtogo = True
+         if grouped_mode:
+            if not set(othernames).issubset(namesG):
+                goodtogo = False
+         if goodtogo:
+             if verbose_mode:
+                print(printable_mode + " game " + str(total + 1) + ":")
+             for h in range(10):
+                username = getName[total*10 + h].find_element_by_tag_name('a').get_attribute('innerHTML')
+                if verbose_mode:
+                    print(username + " " + getScores[total*10 + h].get_attribute('innerHTML'))
+                if names[j].lower() in username.lower():
+                    names[j] = username
+                    totalscore = totalscore + float(getScores[total*10 + h].get_attribute('innerHTML'))
+             total = total + 1
+             if verbose_mode:
+                print("")
+             if limited and limitnum == total:
+                break;
 
-   print("Found " + str(total) + " Flex 5:5 games")
-   print("Aggregate OP.GG score: " + str(totalscore / total))
+   print(names[j] + " aggregate OP.GG score over " + str(total) + " " + printable_mode + " games: " + str(totalscore / total))
    print("")
    print("")
 
